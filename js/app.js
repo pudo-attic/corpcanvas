82755732779fab5bcaaa866f21dc7102d96fe851
Friedrich Lindenberg <friedrich@pudo.org>
2015-01-19 12:02:58 +0100

    angular app skeleton

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+var corpcanvas = angular.module('corpcanvas', []);
+
+
+corpcanvas.controller('AppCtrl', ['$scope', '$location', '$http', '$window',
+  function($scope, $location, $http, $window) {
+  
+}]);
+
+
+corpcanvas.factory('d3', ['$rootScope', function($rootScope) {
+  return {};
+}]);
+
