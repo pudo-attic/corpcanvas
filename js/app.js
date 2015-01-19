d0fb6f4479e87e6cab05806eacedc1059ffc0777
Friedrich Lindenberg <friedrich@pudo.org>
2015-01-19 15:30:24 +0100

    drag and drop of nodes

--- js/app.js
+++ js/app.js
@@ -7,8 +7,8 @@ var FIXTURES = {
 }
 
 
-corpcanvas.controller('AppCtrl', ['$scope', '$location', '$http', '$window', 'graph',
-  function($scope, $location, $http, $window, graph) {
+corpcanvas.controller('AppCtrl', ['$scope', '$location', '$http', '$window', 'graph', 'grid',
+  function($scope, $location, $http, $window, graph, grid) {
   
   var svg = d3.select("#page").append("svg"),
       gridSize = 128;
@@ -18,8 +18,35 @@ corpcanvas.controller('AppCtrl', ['$scope', '$location', '$http', '$window', 'gr
         height = $(window).height();
     svg.attr("width", width)
        .attr("height", height);
+    grid.reset(width, height);
     redraw();
   };
+
+  var dragStarted = function(d) {
+    d3.event.sourceEvent.stopPropagation();
+    d3.select(this).classed("dragging", true);
+  };
+
+  var dragged = function(d) {
+    d3.select(this)
+      .attr("cx", function(d) {
+        d.x = d.x + d3.event.dx;
+        d.x = Math.max(grid.minX(), d.x);
+        d.x = Math.min(grid.maxX(), d.x);
+        return d.x;
+      })
+      .attr("cy", function(d) {
+        d.y = d.y + d3.event.dy;
+        d.y = Math.max(grid.minY(), d.y);
+        d.y = Math.min(grid.maxY(), d.y);
+        return d.y;
+      });
+  };
+
+  var dragEnded = function(d) {
+    d3.select(this)
+      .classed("dragging", false);
+  }
 
   var loadFixtures = function() {
     angular.forEach(FIXTURES.nodes, function(n) {
@@ -33,12 +60,19 @@ corpcanvas.controller('AppCtrl', ['$scope', '$location', '$http', '$window', 'gr
   var redraw = function() {
     graph.dump();
 
+    var drag = d3.behavior.drag()
+      .origin(function(d) { return d; })
+      .on("dragstart", dragStarted)
+      .on("drag", dragged)
+      .on("dragend", dragEnded);
+
     svg.selectAll("circle")
         .data(graph.nodes())
       .enter().append("circle")
-        .attr("r", function(d) { return 30; })
-        .attr("cx", function(d) { return 100 * d.id; })
-        .attr("cy", function(d) { return 100 * d.id; })
+        .attr("r", function(d) { return grid.getFactor(); })
+        .attr("cx", function(d) { d.x = d.id * 100; return d.x; })
+        .attr("cy", function(d) { d.y = d.id * 100; return d.y; })
+        .call(drag)
         .style("fill", function(d, i) { return '#8000aa'; });
 
   };
@@ -53,26 +87,60 @@ corpcanvas.controller('AppCtrl', ['$scope', '$location', '$http', '$window', 'gr
 }]);
 
 
+corpcanvas.factory('grid', ['$rootScope', function($rootScope) {
+  var grid = [],
+      windowWidth = 0,
+      windowHeight = 0,
+      numHorizontal = 0,
+      numVertical = 0,
+      factor = 10;
+
+  return {
+    reset: function(width, height) {
+      windowWidth = width;
+      windowHeight = height;
+      factor = Math.min(1000, Math.max(20, width / 50));
+      padding = factor * 0.3;
+    },
+    getFactor: function() {
+      return factor;
+    },
+    minX: function() {
+      return factor + padding;
+    },
+    minY: function() {
+      return factor + padding;
+    },
+    maxX: function() {
+      return windowWidth - (factor + padding);
+    },
+    maxY: function() {
+      return windowHeight - (factor + padding);
+    }
+  };
+}]);
+
+
 
 corpcanvas.factory('graph', ['$rootScope', function($rootScope) {
   var nodes = {},
       links = [];
 
   return {
-    'addNode': function(data) {
+    addNode: function(data) {
       nodes[data.id] = data;
     },
-    'addLink': function(data) {
+    addLink: function(data) {
       links.push(data);
     },
-    'nodes': function() {
+    nodes: function() {
       var nodeList = [];
       angular.forEach(nodes, function(v) {
         nodeList.push(v);
       });
       return nodeList;
     },
-    'dump': function() {
+    dump: function() {
       console.log("Graph", nodes, links);
     }
   };
